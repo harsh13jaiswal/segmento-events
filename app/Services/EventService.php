ed61d6bccb0a3ae55673a275dda9c8648eee90a8
Harsh <harshjaiswal@whozzat.com>
2023-11-03 09:51:22 +0530

    Harsh|added baseID changes in Delete EventType

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -39,9 +39,9 @@ class EventService{
         return $this->lib->runQuery($query);
     }
 
-    public function deleteEvents($companyId,$id){
+    public function deleteEvents($baseId,$id){
         $query="DELETE FROM via-socket-prod.segmento.event_types
-        WHERE company_id = '$companyId' AND identifier ='$id'";
+        WHERE base_id = '$baseId' AND identifier ='$id'";
         try{
             $this->lib->runQuery($query);
         }catch(Exception $e){
