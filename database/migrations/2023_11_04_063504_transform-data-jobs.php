a5cd92e6d99cb05c2ef99409d247898dc5d33f0a
Harsh <harshjaiswal@whozzat.com>
2023-11-04 19:21:37 +0530

    harsh|setup rabbit mq

---
+++ database/migrations/2023_11_04_063504_transform-data-jobs.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('transform_data_jobs');
+        Schema::create('transform_data_jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('payload');
+            $table->string('integration');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transform_data_jobs');
+    }
+};
