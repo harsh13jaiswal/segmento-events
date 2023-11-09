70d0e5a74e3b0da7c8ee72a6cea3a8c0c5ae276e
Harsh <harshjaiswal@whozzat.com>
2023-11-09 15:49:56 +0530

    harsh|added bulkInsertQueryFunction

--- app/Services/EventLogsService.php
+++ app/Services/EventLogsService.php
@@ -20,8 +20,35 @@ class EventLogsService{
     }
 
     public function executeEventLogJob($data){
-        Storage::disk('local')->put('example.txt', json_encode($data));
-        // $dbconn = new BigqueryLib();
-        // $dbconn->bulkInsert($data);
+       $query=generateBulkEventInsertQuery($data);
+       $this->bigQueryLib->runQueryOnDB($query);
+
+    }
+    
+    public function generateBulkEventInsertQuery($data) {
+        $table="via-socket-prod.segmento.user_events";
+        $query = "INSERT INTO $table 
+        ( `identifier`,`user_id`,`base_id`, `event_name`, `type`, `created_at`, `context`, `page`, `event_timestamp`, `event_properties`) 
+        VALUES ";
+
+        $valueStrings = [];
+        foreach ($data as $row) {
+            $identifier=substr(Str::uuid()->toString(), -10);
+            $base_id = $row['base_id'];
+            $user_id = $row['user_id'];
+            $event_name = $row['event_name'];
+            $type = $row['type'];
+            $created_at = date('Y-m-d H:i:s');
+            $context = json_encode($row['context']);
+            $page = json_encode($row['page']);
+            $event_timestamp = $row['event_timestamp'];
+            $event_properties = json_encode($row['event_properties']);
+
+            $valueStrings[] = "('$identifier','$base_id','$user_id', '$event_name', '$type', TIMESTAMP '$created_at', JSON '$context', JSON '$page', TIMESTAMP '$event_timestamp', JSON '$event_properties')";
+        }
+
+        $query .= implode(', ', $valueStrings);
+
+        return $query;
     }
 }
